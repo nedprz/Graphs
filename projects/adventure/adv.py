eb586f37f8a802d6da7e0bc07da71705b20f07c4
nedprz <16900857+nedprz@users.noreply.github.com>
2020-09-11 10:25:33 -0700

    working with ~1500 steps

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -14,7 +14,7 @@ world = World()
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+map_file = "projects/adventure/maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -28,7 +28,98 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+################################################
 
+
+# we will call visited a set so we don't double count rooms
+
+
+visited = set()  
+
+
+#define variables to be used for directions of walk
+prev = ''
+next = ''
+landlocked = dict()
+
+player.current_room = world.starting_room
+visited.add(player.current_room.id)
+
+#We will walk south to start
+direction = 's'
+traversal_path.append(direction)
+
+#define number of rooms and loop through until they are all visited
+ROOMS = 500
+while len(visited) < ROOMS:
+    player.travel(direction)
+    visited.add(player.current_room.id)
+
+    prev = direction
+
+    # Traversal process 
+
+    if prev == 's':
+        if 'w' in player.current_room.get_exits():
+            direction = 'w'
+        elif 's' in player.current_room.get_exits():
+            direction = 's'
+        elif 'e' in player.current_room.get_exits():
+            direction = 'e'
+        else: 
+            direction = 'n'
+    
+    elif prev == 'n':
+        if 'e' in player.current_room.get_exits():
+            direction = 'e'
+        elif 'n' in player.current_room.get_exits():
+            direction = 'n'
+        elif 'w' in player.current_room.get_exits():
+            direction = 'w'
+        else: 
+            direction = 's'
+    
+    elif prev == 'w':
+        if 'n' in player.current_room.get_exits():
+            direction = 'n'
+        elif 'w' in player.current_room.get_exits():
+            direction = 'w'
+        elif 's' in player.current_room.get_exits():
+            direction = 's'
+        else: 
+            direction = 'e'
+
+    elif prev == 'e':
+        if 's' in player.current_room.get_exits():
+            direction = 's'
+        elif 'e' in player.current_room.get_exits():
+            direction = 'e'
+        elif 'n' in player.current_room.get_exits():
+            direction = 'n'
+        else: 
+            direction = 'w'
+    
+
+    # Takes care of special case where a room is "landlocked" by 4 other rooms, which can lead to an infinite loop 
+    if len(player.current_room.get_exits()) == 4:
+        current = player.current_room.id
+
+        if current not in landlocked:
+            landlocked[current] = []
+        
+        if direction not in landlocked[current]:
+            landlocked[current].append(direction)
+        
+        elif len(landlocked[current]) < 4:
+            direction = random.choice([i for i in ['n', 's', 'e', 'w'] if i not in landlocked[current]])
+            landlocked[current].append(direction)
+        
+        else:
+            direction = landlocked[current][len(landlocked[current])%4] 
+            landlocked[current].append(direction)
+
+    # Add the next movement to the traversal_path
+    traversal_path.append(direction)
 
 
 # TRAVERSAL TEST
@@ -46,17 +137,15 @@ else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
-
-
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+#player.current_room.print_room_description(player)
+#while True:
+#    cmds = input("-> ").lower().split(" ")
+#    if cmds[0] in ["n", "s", "e", "w"]:
+#        player.travel(cmds[0], True)
+#    elif cmds[0] == "q":
+#        break
+#    else:
+#        print("I did not understand that command.")
